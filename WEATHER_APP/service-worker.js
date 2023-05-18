68b03767e92f750cd2eb6c4b89ad71c3bc770f7a
tkuyyt <133862639+tkuyyt@users.noreply.github.com>
2023-05-18 16:43:45 +0800

    txt

--- WEATHER_APP/service-worker.js
+++ WEATHER_APP/service-worker.js
@@ -22,12 +22,12 @@ var filesToCache = [
 
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
-  e.waitUntil(
+  /*e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       console.log('[ServiceWorker] Caching app shell');
       return cache.addAll(filesToCache);
     })
-  );
+  );*/
 });
 
 self.addEventListener('activate', function(e) {
